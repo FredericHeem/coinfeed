edc34f5a61837fde781241b03fb90d7ef6d63e6f
Frederic Heem <frederic.heem@gmail.com>
2013-05-04 19:03:55 +0200

    http://localhost:3000

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ console.log('CoinFeed ')
 var config = require('./config.js')();
 var coinFeedApp = require('./coinFeedApp.js')(config);
 
-//console.log('NODE_ENV ' + app.settings.env)
-
-//app.use(coinFeedApp);
 
 var port = process.env.PORT || 3000
-
+var ip = process.env.IP || "localhost"
 coinFeedApp.listen(port, function() {
-    console.log('listening on http://' + process.env.IP + ':' + port)
+    console.log('listening on http://' + ip + ':' + port)
 });
