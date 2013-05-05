0bf4a7d0f64d6e55d0c8f263dc26b786153eb8ba
Frederic Heem <frederic.heem@gmail.com>
2013-05-05 13:33:23 +0200

    first jqplot

--- assets/js/connected.js
+++ assets/js/connected.js
@@ -1,2 +1,13 @@
-//= require vendor/require-jquery
+//= require vendor/jquery-1.7.1.min
+//= require vendor/bootstrap
+//= require vendor/jquery-ui-1.8.11.custom.min
+//= require vendor/underscore-min
+//= require vendor/backbone-min
+//= require vendor/moment.min
+//= require vendor/jquery.jqplot.min
+//= require vendor/jqplot.dateAxisRenderer.min
+//= require vendor/jqplot.ohlcRenderer.min
+//= require vendor/jqplot.highlighter.min
+//= require vendor/jqplot.json2.min
+//= require coinfeed_client.js
 
